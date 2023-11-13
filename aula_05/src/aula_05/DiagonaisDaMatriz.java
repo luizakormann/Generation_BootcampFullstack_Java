bc6404e1980ca795e4524814b35cd14424bbaec0
unknown <luiza.kormann@gmail.com>
2023-11-13 16:07:18 -0300

    Exercício 3 da aula 5: correção da formatação final pós síncrono em aula

--- aula_05/src/aula_05/DiagonaisDaMatriz.java
+++ aula_05/src/aula_05/DiagonaisDaMatriz.java
@@ -9,39 +9,25 @@ public class DiagonaisDaMatriz {
 
 		int matriz[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 		int el_dig_principal = 0, soma_dig_principal = 0, el_dig_sec = 0, soma_dig_sec = 0;
+		String diagonalP = "", diagonalS = " ";
 
 		for (int linha = 0; linha < matriz.length; linha++) {
 			for (int coluna = 0; coluna < matriz.length; coluna++) {
 				if (linha == coluna) {
 					el_dig_principal = (matriz[linha][coluna]);
 					soma_dig_principal += el_dig_principal;
+					diagonalP += (matriz[linha][coluna]) + ", ";
 				}
 				if (linha + coluna == 2) {
 					el_dig_sec = (matriz[linha][coluna]);
 					soma_dig_sec += el_dig_sec;
+					diagonalS += (matriz[linha][coluna]) + ", ";
 				}
 
 			}
 		}
-
-		// Todos os elementos da Diagonal Principal - C = L
-		System.out.println("Elementos da diagonal principal: ");
-		for (int linha = 0; linha < matriz.length; linha++) {
-			for (int coluna = 0; coluna < matriz.length; coluna++) {
-				if (linha == coluna) {
-				System.out.println(matriz[linha][coluna] + " ");
-				}
-			}
-		}
-		// Todos os elementos da Diagonal Secundária - C + L = 2
-		System.out.println("Elementos da diagonal secundária: ");
-		for (int linha = 0; linha < matriz.length; linha++) {
-			for (int coluna = 0; coluna < matriz.length; coluna++) {
-				if (linha + coluna == 2) {
-				System.out.println(matriz[linha][coluna] + " ");
-				}
-			}
-		}
+		System.out.println("Elementos da Diagonal Principal: " + diagonalP);
+		System.out.println("Elementos da Diagonal Secundária: " + diagonalS);
 		System.out.println("Soma dos elementos da Diagonal Principal: " + soma_dig_principal);
 		System.out.println("Soma dos elementos da Diagonal Secundária: " + soma_dig_sec);
 
