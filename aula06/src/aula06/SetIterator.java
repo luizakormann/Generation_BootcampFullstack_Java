f3aff78510e3de1ef6b88c9d8ef72316e70c3c61
unknown <luiza.kormann@gmail.com>
2023-11-15 15:19:44 -0300

    Exercício 3 da aula 6: exercício refeito para reforço

--- aula06/src/aula06/SetIterator.java
+++ aula06/src/aula06/SetIterator.java
@@ -1,6 +1,7 @@
 package aula06;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -9,19 +10,31 @@ public class SetIterator {
 	static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Set<Integer> numeros = new HashSet<Integer>();
 
+		Set<Integer> set = new HashSet<Integer>();
+
+		int num;
+
+		// O programa deverá solicitar ao usuário, que ele digite via teclado 10 valores
+		// inteiros não repetidos
+		System.out.println(
+				"Boas vindas! Vamos criar uma Collection Set?\nVocê digitará 10 números inteiros, aleatórios e não repetidos.");
+		System.out.println(
+				"Depois o programa mostrará os elementos únicos do seu Set (se você digitar números repetidos, ele mostrará apenas uma vez)");
+		System.out.println("\nPor favor, digite 10 números:");
+		// adicione-os individualmente na Collection Set.
 		for (int i = 0; i < 10; i++) {
-			System.out.println("Digite um numero inteiro: ");
-			int numero = leia.nextInt();
-
-			numeros.add(numero);
+			set.add(leia.nextInt());
 		}
-		System.out.println("Listar Set: ");
-		for (Integer numero : numeros) {
-			System.out.println(numero);
+			
+		//Criando o primeiro Iterator
+		Iterator<Integer> iSet = set.iterator();
+		
+		// Mostre na tela todos os elementos da Collection Set, utilizando Iterator
+		System.out.println("Os números únicos do seu Set são:");
+		while (iSet.hasNext()) {
+			System.out.println(iSet.next());
 		}
-
+		
 	}
-
 }
