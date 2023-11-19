739b08daa8584e16b8d545d416b0cad536a91c2b
unknown <luiza.kormann@gmail.com>
2023-11-19 16:32:55 -0300

    Exercício 1 da aula 8: Prática de POO - integração ingresso <> testes

--- aula_08/src/aula_08/Ingresso.java
+++ aula_08/src/aula_08/Ingresso.java
@@ -1,5 +1,85 @@
 package aula_08;
 
 public class Ingresso {
+	private String evento;
+	private int tipo;
+	private int local;
+	private int secao;
+	private float preco;
+
+	public Ingresso(String evento, int tipo, int local, int secao, float preco) {
+		this.evento = evento;
+		this.tipo = tipo;
+		this.local = local;
+		this.secao = secao;
+		this.preco = preco;
+	}
+
+	public String getEvento() {
+		return evento;
+	}
+
+	public void setEvento(String evento) {
+		this.evento = evento;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getLocal() {
+		return local;
+	}
+
+	public void setLocal(int local) {
+		this.local = local;
+	}
+
+	public int getSecao() {
+		return secao;
+	}
+
+	public void setSecao(int secao) {
+		this.secao = secao;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public void visualizar() {
+		String tipo_evento = "";
+		switch (this.tipo) {
+		case 1 -> tipo_evento = "Show";
+		case 2 -> tipo_evento = "Teatro";
+		case 3 -> tipo_evento = "Cinema";
+		}
+		String cidade = "";
+		switch (this.local) {
+		case 1 -> cidade = "São Paulo";
+		case 2 -> cidade = "Salvador";
+		case 3 -> cidade = "Recife";
+		}
+		String secao_ing = "";
+		switch (this.secao) {
+		case 1 -> secao_ing = "Pista";
+		case 2 -> secao_ing = "Balcão superior";
+		}
+
+		System.out.println("Boas vindas! Confirme os dados de seu evento abaixo: ");
+		System.out.println("Nome do evento: " + this.evento);
+		System.out.println("Tipo do evento: " + tipo_evento);
+		System.out.println("Local do evento: " + cidade);
+		System.out.println("Seção do seu ingresso: " + secao_ing);
+		System.out.println("Valor do ingresso: R$" + this.preco + "\n");
+	}
 
 }
--- aula_08/src/aula_08/TesteIngresso.java
+++ aula_08/src/aula_08/TesteIngresso.java
@@ -3,7 +3,12 @@ package aula_08;
 public class TesteIngresso {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Ingresso i1 = new Ingresso("Rei Leão", 2, 1, 1, 250.00f);
+		i1.visualizar();
+		
+		Ingresso i2 = new Ingresso("Gilsons", 1, 3, 2, 200.00f);
+		i2.visualizar();
 
 	}
 
