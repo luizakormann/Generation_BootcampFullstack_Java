8e77c4b7f59fafa8c45fbea9b3f5a78319b826fc
unknown <luiza.kormann@gmail.com>
2023-11-23 22:59:16 -0300

    complemento exercício com loop

--- aula_exceptions/src/aula_exceptions/Divisao.java
+++ aula_exceptions/src/aula_exceptions/Divisao.java
@@ -6,25 +6,33 @@ import java.util.Scanner;
 public class Divisao {
 
 	static Scanner ler = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
+
 		int dividendo = 0;
 		int divisor = 0;
-		
-		try {
-		System.out.println("Digite o Dividendo: ");
-		dividendo = ler.nextInt();
+		boolean loop = true;
+
+		do {
+			try {
+				System.out.println("Digite o Dividendo: ");
+				dividendo = ler.nextInt();
+
+				System.out.println("Digite o Divisor: ");
+				divisor = ler.nextInt();
+
+				divide(dividendo, divisor);
 				
-		System.out.println("Digite o Divisor: ");
-		divisor = ler.nextInt();
-				
-		divide(dividendo, divisor);
-		}catch(InputMismatchException e) {
-			System.err.println("Erro de entrada: " + e + "\n\nDigite um numeral inteiro (sem vírgula/ponto).");
-		}catch(ArithmeticException e) {
-			System.err.println("Erro de entrada: " + e + "\n\nDigite um número diferente de 0 (não existe divisão por 0).");
-		}
+				loop = false;
+			} catch (InputMismatchException e) {
+				ler.nextLine();
+				System.err.println("Erro de entrada: " + e + "\n\nDigite um numeral inteiro (sem vírgula/ponto).");
+			} catch (ArithmeticException e) {
+				ler.nextLine();
+				System.err.println(
+						"Erro de entrada: " + e + "\n\nDigite um número diferente de 0 (não existe divisão por 0).");
+			}
+		} while (loop);
 	}
 
 	public static void divide(int dividendo, int divisor) {
